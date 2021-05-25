5cdad263de866b46d6ac401f7df0cb2299ec1858
10100programer <alexmettler@hotmail.com>
2021-05-24 23:14:24 -0500

    Adding Visual Studio Project Files and initial interfaces and models

---
+++ Nano-Paste/Nano-Paste-MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Nano_Paste_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
