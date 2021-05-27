8dab70b4bc9854f64a44f6d92943c4ff1d64c1c0
10100programer <alexmettler@hotmail.com>
2021-05-26 21:58:29 -0500

    Basic JSON decoding completed, just need to get from web API next

--- Nano-Paste-Frontend/src/app/pasteview/pasteview.component.ts
+++ Nano-Paste-Frontend/src/app/pasteview/pasteview.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PasteviewComponent implements OnInit {
 
+  test = '{"pasteID":"AE35","pasteData":"Wow this is really Cool\\n\\n\\nReality can be whatever I want","pasteTime":"2021-05-26T21:42:49.5338144-05:00"}';
+  JSONDATA:any;
   constructor() { }
 
   ngOnInit(): void {
+    this.JSONDATA = JSON.parse(this.test);
+    console.log("JSON object -", this.JSONDATA);
   }
 
 }
--- Nano-Paste/Nano-Paste-MVC/Controllers/HomeController.cs
+++ Nano-Paste/Nano-Paste-MVC/Controllers/HomeController.cs
@@ -25,12 +25,9 @@ namespace Nano_Paste_MVC.Controllers
             {
                 List<PasteModel> model = new List<PasteModel>();
                 model.Add(new PasteModel("AE35", "Wow this is really Cool\n\n\nReality can be whatever I want", DateTime.Now));
-                model.Add(new PasteModel("AE36", "Wow this is really Cool\n\n\nReality can be whatever I want", DateTime.Now));
-                model.Add(new PasteModel("AE37", "Wow this is really Cool\n\n\nReality can be whatever I want", DateTime.Now));
-                model.Add(new PasteModel("AE39", "Wow this is really Cool\n\n\nReality can be whatever I want", DateTime.Now));
 
 
-                return Json(model);
+                return Json(model[0]);
                 //return View("ViewPaste", pasteModel);
             }
             else
