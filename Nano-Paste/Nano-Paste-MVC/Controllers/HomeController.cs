8f382cefb1a6931539a13967451e9281aee4f917
10100programer <alexmettler@hotmail.com>
2021-07-14 20:34:28 -0500

    Changed folder structure and angular build settings

--- Nano-Paste/Nano-Paste-MVC/Controllers/HomeController.cs
+++ Nano-Paste/Nano-Paste-MVC/Controllers/HomeController.cs
@@ -18,32 +18,11 @@ namespace Nano_Paste_MVC.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index(string id)
-        {
-            string datahold = string.Empty;
-            if(!String.IsNullOrWhiteSpace(id))
-            {
-                List<PasteModel> model = new List<PasteModel>();
-                model.Add(new PasteModel("AE35", "Wow this is really Cool\n\n\nReality can be whatever I want", DateTime.Now));
-
-
-                return Json(model[0]);
-                //return View("ViewPaste", pasteModel);
-            }
-            else
-            {
-                return View("Index", datahold);
-            }    
-        }
-        public IActionResult Privacy()
-        {
-            return View();
-        } 
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
 }
